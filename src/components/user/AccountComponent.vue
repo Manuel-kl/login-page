fdae0200de67bcce5f26c60dfa852890136255d3
Langat Emmanuel <mannuehkipkirui@gmail.com>
2022-09-06 13:53:18 +0300

    new

---
+++ src/components/user/AccountComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="account-options">
+        <ul class="account-options">
+            <li>Profile</li>
+            <li>Account Settings</li>
+            <li>Privacy and Security</li>
+            <li>Log Out</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    components: {},
+    props: {},
+    data() {
+        return {};
+    },
+    created() { },
+    methods: {},
+    mounted() { },
+};
+</script>
+<style scoped>
+</style>
+<style lang='scss' scoped>
+</style>
